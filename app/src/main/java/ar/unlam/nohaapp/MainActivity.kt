7e0ca1e1ecfc27ed9a5c304411d408f7d77d8620
suracelucas <surace.lucas2019@gmail.com>
2021-06-18 21:11:41 -0300

    Se agrega el recyclerview

--- app/src/main/java/ar/unlam/nohaapp/MainActivity.kt
+++ app/src/main/java/ar/unlam/nohaapp/MainActivity.kt
@@ -1,11 +1,38 @@
 package ar.unlam.nohaapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import ar.unlam.nohaapp.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding  = ActivityMainBinding.inflate(LayoutInflater.from(this))
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+        binding.diaSemana.text = ""
+        if(binding.diaSemana.text == "") {
+            binding.diaSemana.text = getString(getDay())
+        }
+        setUpView()
+    }
+
+    private fun setUpView() {
+        val myDataSet = Datasource().loadAffirmations()
+        binding.novedades.adapter = ItemsAdapter(this, myDataSet)
+    }
+
+    fun getDay():Int{
+        return when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+            1->R.string.domingo
+            2->R.string.lunes
+            3->R.string.martes
+            4->R.string.miercoles
+            5->R.string.jueves
+            6->R.string.viernes
+            else-> R.string.sabado
+        }
     }
 }
