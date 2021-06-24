225e19986e29e060e56e8617e7805a7e3e0b4b82
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2021-06-23 21:22:21 -0300

    Cambios en la estructura de datos

--- app/src/main/java/ar/unlam/nohaapp/NotificacionesActivity.kt
+++ app/src/main/java/ar/unlam/nohaapp/NotificacionesActivity.kt
@@ -4,23 +4,23 @@ import android.os.Bundle
 import android.widget.ExpandableListAdapter
 import android.widget.ExpandableListView
 import androidx.appcompat.app.AppCompatActivity
-import ar.unlam.nohaapp.NotificacionesData.data
 
 class NotificacionesActivity : AppCompatActivity() {
     private var expandableListView: ExpandableListView? = null
     private var adapter: ExpandableListAdapter? = null
     private var lugarList: List<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notificaciones)
+        title = "Configuración de notificaciones"
         expandableListView = findViewById(R.id.eListView)
 
         if (expandableListView != null) {
-            val listData = data
+            val listData = Datasource().loadLugares()
             lugarList = ArrayList(listData.keys)
             adapter = NotificacionesListAdapter(this, lugarList as ArrayList<String>, listData)
             expandableListView!!.setAdapter(adapter)
-
         }
     }
 }
