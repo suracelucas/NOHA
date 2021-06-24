3f1d8a896eca687cdbddc5ef587ec10d91091fe9
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2021-06-23 21:25:15 -0300

    Cambios en el adapter para matchear con el nuevo formato de datos, los switchs ahora están ligados a su evento

--- app/src/main/java/ar/unlam/nohaapp/NotificacionesListAdapter.kt
+++ app/src/main/java/ar/unlam/nohaapp/NotificacionesListAdapter.kt
@@ -12,26 +12,27 @@ import com.google.android.material.switchmaterial.SwitchMaterial
 class NotificacionesListAdapter internal constructor(
     private val context: NotificacionesActivity,
     private val lugarList: List<String>,
-    private val eventosList: HashMap<String, List<String>>
+    private val eventosList: HashMap<String, List<Evento>>
 ) : BaseExpandableListAdapter() {
 
-    override fun getChild(listPosition: Int, expandedListPosition: Int): Any {
-        return this.eventosList[this.lugarList[listPosition]]!![expandedListPosition]
+    override fun getChild(listPosition: Int, childPosition: Int): Any {
+        return eventosList[lugarList[listPosition]]!![childPosition]
     }
 
-    override fun getChildId(listPosition: Int, expandedListPosition: Int): Long {
-        return expandedListPosition.toLong()
+    override fun getChildId(listPosition: Int, childPosition: Int): Long {
+        return childPosition.toLong()
     }
 
     override fun getChildView(
         listPosition: Int,
-        expandedListPosition: Int,
+        childPosition: Int,
         isLastChild: Boolean,
         convertView: View?,
-        parent: ViewGroup
+        parent: ViewGroup,
     ): View {
         var convertedView = convertView
-        val expandedListText = getChild(listPosition, expandedListPosition) as String
+        val nombreEvento = eventosList[lugarList[listPosition]]!![childPosition].nombreEvento
+        var notificar = eventosList[lugarList[listPosition]]!![childPosition].notificar
 
         if (convertedView == null) {
             val layoutInflater =
@@ -39,39 +40,55 @@ class NotificacionesListAdapter internal constructor(
             convertedView = layoutInflater.inflate(R.layout.eventos_list, parent, false)
         }
 
-        val expandedListTextView = convertedView!!.findViewById<TextView>(R.id.eventos_tv)
-        expandedListTextView.text = expandedListText
+        val switch: SwitchMaterial = convertedView!!.findViewById(R.id.switch_noti)
 
-        val switch: SwitchMaterial = convertedView.findViewById(R.id.switch_noti)
+        switch.isChecked = notificar
         switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
+                notificar = true
                 Toast.makeText(
                     context,
-                    "Notificar $expandedListText: SI",
+                    "Notificar $nombreEvento: SI \n ParentID: ${getGroupId(listPosition)} \n ChildID: ${
+                        getChildId(
+                            listPosition,
+                            childPosition
+                        )
+                    } \n Notificar: $notificar",
                     Toast.LENGTH_SHORT
                 ).show()
+
             } else {
+                notificar = false
                 Toast.makeText(
                     context,
-                    "Notificar $expandedListText: NO",
+                    "Notificar $nombreEvento: NO \n ParentID: ${getGroupId(listPosition)} \n ChildID: ${
+                        getChildId(
+                            listPosition,
+                            childPosition
+                        )
+                    } \n Notificar: $notificar",
                     Toast.LENGTH_SHORT
                 ).show()
+
             }
         }
+
+        val childTextView = convertedView.findViewById<TextView>(R.id.eventos_tv)
+        childTextView.text = nombreEvento
 
         return convertedView
     }
 
     override fun getChildrenCount(listPosition: Int): Int {
-        return this.eventosList[this.lugarList[listPosition]]!!.size
+        return eventosList[lugarList[listPosition]]!!.size
     }
 
     override fun getGroup(listPosition: Int): Any {
-        return this.lugarList[listPosition]
+        return lugarList[listPosition]
     }
 
     override fun getGroupCount(): Int {
-        return this.lugarList.size
+        return lugarList.size
     }
 
     override fun getGroupId(listPosition: Int): Long {
@@ -86,21 +103,24 @@ class NotificacionesListAdapter internal constructor(
     ): View {
         var convertedView = convertView
         val listTitle = getGroup(listPosition) as String
+
         if (convertedView == null) {
             val layoutInflater =
                 this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             convertedView = layoutInflater.inflate(R.layout.lugar_list, parent, false)
         }
+
         val listTitleTextView = convertedView!!.findViewById<TextView>(R.id.lugar_tv)
         listTitleTextView.text = listTitle
+
         return convertedView
     }
 
     override fun hasStableIds(): Boolean {
-        return false
+        return true
     }
 
-    override fun isChildSelectable(listPosition: Int, expandedListPosition: Int): Boolean {
+    override fun isChildSelectable(listPosition: Int, childPosition: Int): Boolean {
         return true
     }
 
