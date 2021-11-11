71bc98332472638f48d6f2f286a841a28ad75375
Luciano de la Iglesia <l.delaiglesia@hotmail.com>
2021-11-11 00:09:08 -0300

    cambio en colores y fuente de menu activity

--- app/src/main/java/ar/unlam/nohaapp/ui/theme/Theme.kt
+++ app/src/main/java/ar/unlam/nohaapp/ui/theme/Theme.kt
@@ -7,15 +7,16 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Second,
+    primaryVariant = SecondDark,
+    secondary = SecondLight
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Orange,
+    primaryVariant = OrangeDark,
+    secondary = OrangeLight
+
 
     /* Other default colors to override
     background = Color.White,
